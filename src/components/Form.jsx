d0fdef978178aafeba7f76504e45705d221bd86e
Damon Haase <damon.haase@gmail.com>
2024-06-20 12:52:40 -0400

    Add additional sections

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,32 @@
 import FormSection from "./FormSection";
 
 export default function Form() {
-  const genInputs = [
+  const genInfoInputs = [
     { name: "First Name" },
     { name: "Last Name" },
     { name: "Email", type: "email" },
     { name: "Phone Number", type: "tel" },
   ];
+
+  const educationInputs = [
+    { name: "School Name" },
+    { name: "Title of Study" },
+    { name: "Date of Study", type: "date" },
+  ];
+
+  const workExperienceInputs = [
+    { name: "Company Name" },
+    { name: "Position Title" },
+  ];
+
   return (
     <div className="form">
-      <FormSection title="General Info" inputs={genInputs}></FormSection>
+      <FormSection title="General Info" inputs={genInfoInputs}></FormSection>
+      <FormSection title="Education" inputs={educationInputs}></FormSection>
+      <FormSection
+        title="Work Experience"
+        inputs={workExperienceInputs}
+      ></FormSection>
     </div>
   );
 }
